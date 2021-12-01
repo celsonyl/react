937db87b662021b7f83aa3a803e401511730fe2f
Celso Bertolotto <celsonyl@hotmail.com>
2021-12-01 16:50:54 -0300

    new file routes.js and book

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import React, { useState } from 'react';
-import Login from './pages/Login';
+import React from 'react';
+import Routes from './routes';
 import './global.css'; 
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
-
-  function increment() {
-    setCounter(counter + 1);
-  }
   return (
-    <div>
-      <Login />
-    </div>
+      <Routes />
   );
 }
