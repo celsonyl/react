d84fad5e50d2813aa1ad1e08a6d4ad95d8377d48
Celso Bertolotto <celsonyl@hotmail.com>
2021-12-01 19:52:37 -0300

    WIP list books

--- src/pages/Book/index.js
+++ src/pages/Book/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './styles.css';
 import { Link } from "react-router-dom";
-import {FiPower} from 'react-icons/fi';
+import { FiPower, FiEdit, FiTrash2 } from 'react-icons/fi';
 
 import logo from "../../assets/logo.svg"
 
@@ -9,13 +9,73 @@ export default function Book() {
     return (
         <div className="book-container">
             <header>
-                <img src={logo} alt="Celso"/>
+                <img src={logo} alt="Celso" />
                 <span>Welcome, <strong>Celso</strong>!</span>
                 <Link className="button" to="book/new">Add new Book</Link>
                 <button type="button">
-                    <FiPower size={18} color="#251FC5"/>
+                    <FiPower size={18} color="#251FC5" />
                 </button>
             </header>
+
+            <h1>Registered books</h1>
+            <ul>
+                <li>
+                    <strong>Title:</strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author:</strong>
+                    <p>Nigel</p>
+                    <strong>Price:</strong>
+                    <p>R$ 47.90</p>
+                    <strong>Realease Date:</strong>
+                    <p>12/07/2020</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5" />
+                    </button>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5" />
+                    </button>
+                </li>
+
+                <li>
+                    <strong>Title:</strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author:</strong>
+                    <p>Nigel</p>
+                    <strong>Price:</strong>
+                    <p>R$ 47.90</p>
+                    <strong>Realease Date:</strong>
+                    <p>12/07/2020</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5" />
+                    </button>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5" />
+                    </button>
+                </li>
+
+                <li>
+                    <strong>Title:</strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author:</strong>
+                    <p>Nigel</p>
+                    <strong>Price:</strong>
+                    <p>R$ 47.90</p>
+                    <strong>Realease Date:</strong>
+                    <p>12/07/2020</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5" />
+                    </button>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5" />
+                    </button>
+                </li>
+            </ul>
         </div>
     )
 }
