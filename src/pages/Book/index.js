2fb746067ce27e2caabd474a292914ec78c4dd67
Celso Bertolotto <celsonyl@hotmail.com>
2021-12-01 20:11:24 -0300

    add css in /book

--- src/pages/Book/index.js
+++ src/pages/Book/index.js
@@ -75,6 +75,25 @@ export default function Book() {
                         <FiTrash2 size={20} color="#251FC5" />
                     </button>
                 </li>
+
+                <li>
+                    <strong>Title:</strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author:</strong>
+                    <p>Nigel</p>
+                    <strong>Price:</strong>
+                    <p>R$ 47.90</p>
+                    <strong>Realease Date:</strong>
+                    <p>12/07/2020</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5" />
+                    </button>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5" />
+                    </button>
+                </li>
             </ul>
         </div>
     )
