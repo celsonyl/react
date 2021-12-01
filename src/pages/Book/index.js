2e86493a7fa0b4650fdf28ffec5bcaf8f19a8edf
Celso Bertolotto <celsonyl@hotmail.com>
2021-12-01 17:05:55 -0300

    edit styles.css in /book

--- src/pages/Book/index.js
+++ src/pages/Book/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import './styles.css';
+import { Link } from "react-router-dom";
+import {FiPower} from 'react-icons/fi';
+
+import logo from "../../assets/logo.svg"
 
 export default function Book() {
-    return <h1>Book</h1>
+    return (
+        <div className="book-container">
+            <header>
+                <img src={logo} alt="Celso"/>
+                <span>Welcome, <strong>Celso</strong>!</span>
+                <Link className="button" to="book/new">Add new Book</Link>
+                <button type="button">
+                    <FiPower size={18} color="#251FC5"/>
+                </button>
+            </header>
+        </div>
+    )
 }
